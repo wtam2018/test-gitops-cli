c335e4d289893ab048d00eeb212caccbdc1187f7
William Tam <email.wtam@gmail.com>
2020-09-15 00:58:26 -0400

    add version  sub command

---
+++ pkg/cmd/version/version.go
@@ -0,0 +1,24 @@
+package version
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// RecommendedCommandName is the recommended environment command name.
+const RecommendedCommandName = "version"
+
+var Version string
+
+// NewCmd creates a new environment command
+func NewCmd(name, fullName string) *cobra.Command {
+	return &cobra.Command{
+		Use:   name,
+		Short: "Get version",
+		Long:  "Get command version",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Version: %s\n", Version)
+		},
+	}
+}
